80481bcab084451d5cdcddfdf1fa32a832e1482a
hariram <harisb4160176@gmail.com>
2026-03-22 20:24:39 +0100

    Changes in Extracting Static TF

--- src/thesis_pp_develop/thesis_pp_develop/extract_static_tf.py
+++ src/thesis_pp_develop/thesis_pp_develop/extract_static_tf.py
@@ -7,8 +7,6 @@ import rosbag2_py
 
 BAG_PATH = '/root/UVC_ws/vf_robot_model_ros2/thesis_fisheye_bag3'
 TF_STATIC_TOPIC = '/tf_static'
-SOURCE_FRAME = 'camera_fisheye_front_optical_frame'
-TARGET_FRAME = 'base_link'
 
 storage_options = rosbag2_py.StorageOptions(uri=BAG_PATH, storage_id='sqlite3')
 converter_options = rosbag2_py.ConverterOptions(
@@ -27,34 +25,54 @@ reader.set_filter(storage_filter)
 
 msg_type = get_message(type_map[TF_STATIC_TOPIC])
 
-transform_found = None
+tf_map = {}
 
 while reader.has_next():
     topic, data, timestamp = reader.read_next()
-
     msg = deserialize_message(data, msg_type)
-
     for tf in msg.transforms:
-        print(f'{tf.header.frame_id}  -->  {tf.child_frame_id}')
+        parent = tf.header.frame_id.lstrip('/')
+        child = tf.child_frame_id.lstrip('/')
+        tf_map[(parent, child)] = tf.transform
 
-if transform_found is None:
-    print(f'Transform not found between {SOURCE_FRAME} and {TARGET_FRAME}')
-else:
-    t = transform_found.transform.translation
-    q = transform_found.transform.rotation
 
-    print(f'Parent frame : {transform_found.header.frame_id}')
-    print(f'Child frame  : {transform_found.child_frame_id}')
-    print(f'Translation  : x={t.x:.6f}  y={t.y:.6f}  z={t.z:.6f}')
-    print(f'Quaternion   : x={q.x:.6f}  y={q.y:.6f}  z={q.z:.6f}  w={q.w:.6f}')
-
-    # Convert quaternion to rotation matrix
+def quat_to_rotation_matrix(q):
     qx, qy, qz, qw = q.x, q.y, q.z, q.w
-
-    R = np.array([
-        [1 - 2*(qy**2 + qz**2),   2*(qx*qy - qz*qw),   2*(qx*qz + qy*qw)],
-        [2*(qx*qy + qz*qw),   1 - 2*(qx**2 + qz**2),   2*(qy*qz - qx*qw)],
-        [2*(qx*qz - qy*qw),   2*(qy*qz + qx*qw),   1 - 2*(qx**2 + qy**2)]
+    return np.array([
+        [1 - 2*(qy**2 + qz**2),  2*(qx*qy - qz*qw),  2*(qx*qz + qy*qw)],
+        [2*(qx*qy + qz*qw),  1 - 2*(qx**2 + qz**2),  2*(qy*qz - qx*qw)],
+        [2*(qx*qz - qy*qw),  2*(qy*qz + qx*qw),  1 - 2*(qx**2 + qy**2)]
     ])
 
-    print(f'\nRotation matrix:\n{R}')
+
+def tf_to_matrix(transform):
+    R = quat_to_rotation_matrix(transform.rotation)
+    t = np.array([transform.translation.x,
+                  transform.translation.y,
+                  transform.translation.z])
+    T = np.eye(4)
+    T[:3, :3] = R
+    T[:3, 3] = t
+    return T
+
+
+# T: base_link -> camera_fisheye_front_link
+T1 = tf_to_matrix(tf_map[('base_link', 'camera_fisheye_front_link')])
+
+# T: camera_fisheye_front_link -> camera_fisheye_front_optical_frame
+T2 = tf_to_matrix(tf_map[('camera_fisheye_front_link', 'camera_fisheye_front_optical_frame')])
+
+# T: base_link -> camera_fisheye_front_optical_frame
+T_base_to_optical = T1 @ T2
+
+# Invert to get: camera_fisheye_front_optical_frame -> base_link
+T_optical_to_base = np.linalg.inv(T_base_to_optical)
+
+print('T base_link -> optical_frame:')
+print(T_base_to_optical)
+print('\nT optical_frame -> base_link:')
+print(T_optical_to_base)
+print('\nRotation matrix (optical -> base_link):')
+print(T_optical_to_base[:3, :3])
+print('\nTranslation (optical -> base_link):')
+print(T_optical_to_base[:3, 3])
